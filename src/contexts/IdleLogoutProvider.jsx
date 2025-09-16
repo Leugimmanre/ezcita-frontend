09a3575fe67b687c1966e5ad7c147ed19b7f1b6b
Miguel Martínez <leugimmanre@gmail.com>
2025-09-16 14:38:20 +0200

    "wheel",

--- src/contexts/IdleLogoutProvider.jsx
+++ src/contexts/IdleLogoutProvider.jsx
@@ -160,6 +160,7 @@ export default function IdleLogoutProvider({
       "keydown",
       "scroll",
       "touchstart",
+      "wheel",
       "mousemove",
       "focus",
       "visibilitychange",
